446d303e1282a71cbd9c5aa8e8bce73c77436205
Vova Porton <vova@500tech.com>
2019-01-31 08:50:21 +0200

    Update lib path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native'
 
 export default Platform.select({
-  ios: require('lib/slick-slider.ios'),
-  android: require('lib/slick-slider.android')
+  ios: require('./lib/slick-slider.ios'),
+  android: require('./lib/slick-slider.android')
 });
