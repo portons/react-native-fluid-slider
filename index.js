4f3557977bf645b77bcc58964c6d6fa2cb5b6e10
Vova Porton <vova@500tech.com>
2019-01-31 08:59:23 +0200

    Fix require path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native'
 
 export default Platform.select({
-  ios: require('./lib/slick-slider.ios.js'),
-  android: require('./lib/slick-slider.android.js')
+  ios: require('./lib/slick-slider.ios').default,
+  android: require('./lib/slick-slider.android').default
 });
